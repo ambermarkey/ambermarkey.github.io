7e9fafc73bc184c5e2166a7ffc15e7db354abe66
ambermarkey <57023228+ambermarkey@users.noreply.github.com>
2022-03-09 16:27:04 -0600

    update web-bluetooth

--- web-bluetooth-connect.js
+++ web-bluetooth-connect.js
@@ -1,4 +1,4 @@
-navigator.bluetooth.requestDevice({
+/* navigator.bluetooth.requestDevice({
     filters: [{services: ['2bc61341-2291-4a80-bc74-21bf4c6de00f']}]
 })
     .then(function (device) {
@@ -12,4 +12,70 @@ navigator.bluetooth.requestDevice({
     })
     .catch(function (error) {
         console.error('Connection Failed!',error);
+    }); */
+
+const button = document.getElementById("getDetails");
+const details = document.getElementById("details");
+
+button.addEventListener("click", async () => {
+  try {
+    // Request the Bluetooth device through browser
+    const device = await navigator.bluetooth.requestDevice({
+      optionalServices: ["battery_service", "device_information"],
+      acceptAllDevices: true,
     });
+
+    // Connect to the GATT server
+    // We also get the name of the Bluetooth device here
+    let deviceName = device.gatt.device.name;
+    const server = await device.gatt.connect();
+
+    // Getting the services we mentioned before through GATT server
+    const batteryService = await server.getPrimaryService("battery_service");
+    const infoService = await server.getPrimaryService("device_information");
+
+    // Getting the current battery level
+    const batteryLevelCharacteristic = await batteryService.getCharacteristic(
+      "battery_level"
+    );
+    // Convert recieved buffer to number
+    const batteryLevel = await batteryLevelCharacteristic.readValue();
+    const batteryPercent = await batteryLevel.getUint8(0);
+
+    // Getting device information
+    // We will get all characteristics from device_information
+    const infoCharacteristics = await infoService.getCharacteristics();
+
+    console.log(infoCharacteristics);
+
+    let infoValues = [];
+
+    const promise = new Promise((resolve, reject) => {
+      infoCharacteristics.forEach(async (characteristic, index, array) => {
+        // Returns a buffer
+        const value = await characteristic.readValue();
+        console.log(new TextDecoder().decode(value));
+        // Convert the buffer to string
+        infoValues.push(new TextDecoder().decode(value));
+        if (index === array.length - 1) resolve();
+      });
+    });
+
+    promise.then(() => {
+      console.log(infoValues);
+      // Display all the information on the screen
+      // use innerHTML
+      details.innerHTML = `
+      Device Name - ${deviceName}<br />
+      Battery Level - ${batteryPercent}%<br />
+      Device Information:
+      <ul>
+        ${infoValues.map((value) => `<li>${value}</li>`).join("")}
+      </ul> 
+    `;
+    });
+  } catch (err) {
+    console.log(err);
+    alert("An error occured while fetching device details");
+  }
+});
